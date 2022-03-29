db88ee2d3aa5a212958a82402a5e468d1a893ae3
NamYoundae <youndae8@naver.com>
2022-03-29 14:00:01 +0900

    test commit

---
+++ src/main/java/com/test/BoardProjectJpa/BoardProjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.test.BoardProjectJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectJpaApplication.class, args);
+	}
+
+}
